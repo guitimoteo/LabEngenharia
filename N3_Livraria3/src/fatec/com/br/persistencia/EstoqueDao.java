f1484df325f593f8d8942b5c0863fd204006a531
Guilherme <guitimoteo@yahoo.com.br>
2014-05-16 01:39:41 -0300

    DAO para a classe estoque. Observação, no diagrama o EstoqueDao está como interface.

---
+++ N3_Livraria3/src/fatec/com/br/persistencia/EstoqueDao.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.com.br.persistencia;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author gregory
+ */
+public class EstoqueDao implements DAO<Object>{
+
+    @Override
+    public boolean inserir(Object obj) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean alterar(Object obj) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean excluir(Object obj) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object pesquisar(int pk) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List listar(String criterio) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
